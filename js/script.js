4b0d20c91598f3192c80aa80ba96fa55f0440a5a
marcocareddu <saldumaccu@tuta.io>
2023-05-30 16:41:14 +0200

    JS - Removed unnecessary brackets & renamed var

--- js/script.js
+++ js/script.js
@@ -5,25 +5,24 @@ console.log('JS OK');
 const blocksElement = document.getElementById('blocks-box');
 
 // Declare Calc Variables
-const divisibileBy3 = 3;
-const divisibileBy5 = 5;
-
+const dividendFizz = 3;
+const dividendBuzz = 5;
 
 
 // * PHASE 2 ******************
 // FOR Calc cicle
 for (let i = 1; i <= 100; i++) {
-    if (((i % divisibileBy5) === 0) && ((i % divisibileBy3) === 0)) {
+    if (i % dividendBuzz === 0 && i % dividendFizz === 0) {
         console.log('FizzBuzz');
         // Add Div into DOM
         const tag = '<div class="box fizzbuzz">FizzBuzz</div>';
         blocksElement.innerHTML += tag;
-    } else if ((i % divisibileBy3) === 0) {
+    } else if ((i % dividendFizz) === 0) {
         console.log('Fizz');
         // Add Div into DOM
         const tag = '<div class="box fizz">Fizz</div>';
         blocksElement.innerHTML += tag;
-    } else if ((i % divisibileBy5) === 0) {
+    } else if ((i % dividendBuzz) === 0) {
         console.log('Buzz');
         // Add Div into DOM
         const tag = '<div class="box buzz">Buzz</div>';
