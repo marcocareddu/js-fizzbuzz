6e5f188c2599c05aea2d9c100b360868ad0d79a8
marcocareddu <saldumaccu@tuta.io>
2023-05-30 15:20:25 +0200

    JS - Declared Calc Variables

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ console.log('JS OK');
 // Link to DOM element
 const blocksElement = document.getElementById('blocks-box');
 
+// Declare Calc Variables
+const divisibileBy3 = 3;
+const divisibileBy5 = 5;
